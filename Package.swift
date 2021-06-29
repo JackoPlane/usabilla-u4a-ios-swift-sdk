8ddac35bfad53fcdb230837d5980d3ff41b687c7
Jack Perry <jack.perry@me.com>
2021-06-29 18:18:05 +1000

    Resolve issue with packaging dSYMs

--- Package.swift
+++ Package.swift
@@ -3,10 +3,6 @@
 
 import PackageDescription
 
-let version = "6.8.2"
-let checksum = "d3e5ac0a74a178796f94dfe171d26d6d2dea76b342de7432bd765820a51276fe"
-let url = "https://github.com/usabilla/usabilla-u4a-ios-swift-sdk/releases/download/v\(version)/UsabillaXCFramework.zip"
-
 let package = Package(
     name: "Usabilla",
     platforms: [.iOS(.v9)],
@@ -16,9 +12,9 @@ let package = Package(
             targets: ["Usabilla"]),
     ],
     targets: [
-        .binaryTarget(
-            name: "Usabilla",
-            url: url,
-            checksum: checksum)
+		.binaryTarget(
+			name: "Usabilla",
+			path: "Usabilla.xcframework"
+		)
     ]
 )
